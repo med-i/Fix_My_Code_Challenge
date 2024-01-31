d24ca2375e5b6aedc7f63b688832dfcd8efff4d6
Mohammed Ijairi <mohammed.ijairi@gmail.com>
2024-01-31 01:13:48 +0100

    Fix task 1

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -13,16 +13,16 @@ class square:
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """Initialize a square"""
+        """Initializes a square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
-        """Area of the square"""
+        """Returns the area of the square"""
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """Perimter of the square"""
+    def permiter_of_my_square(self):
+        """Returns the perimter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
